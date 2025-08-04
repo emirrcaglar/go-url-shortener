ef1cc2bf963bb36c2df4b8a1f1f3e07bf9156a5f
emirrcaglar <emirrcaglar@gmail.com>
2025-08-04 16:48:46 +0300

    add loop & create user type

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,16 @@
-DROP TABLE IF EXISTS url;
-CREATE TABLE url (
+DROP TABLE IF EXISTS urls;
+CREATE TABLE urls (
     id INT AUTO_INCREMENT NOT NULL,
     long_url varchar(255) NOT NULL,
+    userID INT,
+    FOREIGN KEY (userID) REFERENCES users(id),
     PRIMARY KEY (id)
 ) AUTO_INCREMENT = 1000000 ;
 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT NOT NULL,
+    username varchar(255) UNIQUE NOT NULL,
+    userpass varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
--- main.go
+++ main.go
@@ -13,20 +13,27 @@ func isValidUrl(inputUrl string) bool {
 }
 
 func main() {
-	var inputUrl string
+	for {
+		var inputUrl string
 
-	fmt.Println("Enter the url you want to shorten.")
-	fmt.Scan(&inputUrl)
+		fmt.Println("Enter the url you want to shorten.")
 
-	if !isValidUrl(inputUrl) {
-		fmt.Printf("Please enter a valid url.")
-		return
+		fmt.Scan(&inputUrl)
+		if inputUrl == "q" {
+			fmt.Println("goodbye.")
+			break
+		}
+
+		if !isValidUrl(inputUrl) {
+			fmt.Printf("Please enter a valid url.\n")
+			continue
+		}
+
+		url := &utils.Url{}
+		baseUrl := "short.ly/"
+
+		shortUrl := utils.GenerateShortUrl(url, inputUrl, baseUrl)
+
+		fmt.Printf("Your short url is: %s\n", shortUrl)
 	}
-
-	url := &utils.Url{}
-	baseUrl := "short.ly/"
-
-	shortUrl := utils.GenerateShortUrl(url, inputUrl, baseUrl)
-
-	fmt.Printf("Your short url is: %s\n", shortUrl)
 }
